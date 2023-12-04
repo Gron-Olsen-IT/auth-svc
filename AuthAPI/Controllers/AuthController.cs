7ff03174ea676d723c4c6bc5956e62aa84947f42
Boes24 <cboes2402@gmail.com>
2023-12-04 21:09:20 +0100

    Log change, auth and verify modified

--- AuthAPI/Controllers/AuthController.cs
+++ AuthAPI/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using AuthAPI.InfraRepo;
 using AuthAPI.Services;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AuthAPI.Controllers;
 
@@ -18,27 +19,33 @@ public class AuthController : ControllerBase
         _AuthService = IAuthService;
     }
 
+    [AllowAnonymous]
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] Authorization auth)
     {
         try
-        {
+        {   
+            _logger.LogInformation("Login attempt");
             return Ok(await _AuthService.ValidateUser(auth.Email!, auth.Password!));
         }
         catch (Exception e)
         {
+            _logger.LogError(e.Message);
             return BadRequest(e.Message);
         }
     }
+    [AllowAnonymous]
     [HttpPost("verify")]
     public async Task<IActionResult> Verify([FromBody] string token)
     {
         try
         {
+            _logger.LogInformation("Verify attempt");
             return Ok(await _AuthService.verifyToken(token));
         }
         catch (Exception e)
         {
+            _logger.LogError(e.Message);
             return BadRequest(e.Message);
         }
     }
